eae54f1353cdb1d9b355ba0067c980738ff6c98e
Aleksander Augustyn <aleksander.augustyn@ncbj.gov.pl>
2026-05-13 18:43:51 +0200

    feat: pymodule entry point registers saddle submodule

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,13 @@
 //!
 //! Public Python API: `pes_analyzer.saddle.find_iwf_grid`.
 
+use pyo3::prelude::*;
+
 mod common;
 mod saddle;
+
+#[pymodule]
+fn pes_analyzer(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    saddle::register(m)?;
+    Ok(())
+}
